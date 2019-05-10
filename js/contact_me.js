825d728ab0d4e8074051064137e90910c38a902a
Luis carlos <hernandezluiscarlos0019@gmail.com>
2019-05-10 08:27:56 -0500

    edicion de bloques

--- js/contact_me.js
+++ js/contact_me.js
@@ -35,7 +35,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>Su mensaje a ha sido envidao.Espere su respuesta pronto </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
